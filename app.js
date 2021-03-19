6db18278235ccb7ae9a38b0794ee8523d31f2e6b
CoderChirag <chirag0174.be20@chitkara.edu.in>
2021-03-19 14:09:31 +0530

    Add NavBar Routing

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.render("index");
 });
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+app.get('/cart', (req, res) => {
+    res.render('cart');
+});
 
-
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server started at port 3000");
 });
+
